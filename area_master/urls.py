8f86aab935a1788309d1f5d7dc742af3222d8a13
Vincent-Juma <vincentjuma64@gmail.com>
2021-05-10 18:46:36 +0300

    clear migration error

--- area_master/urls.py
+++ area_master/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-
 from django.conf.urls import url, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
